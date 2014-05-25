3b66d55d3ec7aafcbef45ef087a62f51ef920236
Pau Picas Sans <pau.picas@gmail.com>
2014-05-25 12:23:11 +0200

    Simplified MainActivity removing menu inflation

--- sample/src/main/java/cat/picas/customtypeface/sample/MainActivity.java
+++ sample/src/main/java/cat/picas/customtypeface/sample/MainActivity.java
@@ -4,8 +4,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.AttributeSet;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 import cat.picas.customtypeface.CustomTypeface;
@@ -22,24 +20,5 @@ public class MainActivity extends Activity {
     public View onCreateView(String name, Context context, AttributeSet attrs) {
         // Delegate the view create to CustomTypeface
         return CustomTypeface.getInstance().createView(name, context, attrs);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
